8fd5953c660db69f67c31a6c30d76fb3940935e4
Nikhil C <78904066+NikhilC-Accolite@users.noreply.github.com>
2021-02-27 19:10:20 +0530

    Update userLogin.js

--- React native - MorningAndAfternoon/Components/userLogin.js
+++ React native - MorningAndAfternoon/Components/userLogin.js
@@ -1,25 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { ImageBackground,Text,TextInput,StyleSheet,View, Button } from "react-native";
 import AsyncStorage from "@react-native-community/async-storage";
-//import PasswordInputText from 'react-native-hide-show-password-input';
 
 const image1 = {uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6wtuVVaD0JKjnKf5QO5EU9MEPMb0vvnIw4g&usqp=CAU"};
-//const image2  = {uri: "https://previews.123rf.com/images/scenery1/scenery11804/scenery1180400457/100119045-bokeh-white-background-color-christmas-and-new-year-theme-background.jpg"};
-
-const userLogin = ({navigation}) => {
+const userLogin = ({navigation}) => 
+{
     const [userName,setUserName] = useState("");
     const [password,setPassword] = useState("");
     const [error,setError] = useState("");
 
-    
-
     const saveData = async () => {
-        try {
+        try
+        {
           await AsyncStorage.setItem("user", userName)
           alert('Data stored successfully')
           navigation.navigate("Add and print")
-
-        } catch (e) {
+        } catch (e) 
+        {
           alert('Failed to save the data to the storage')
         }
       }
@@ -30,14 +27,11 @@ const userLogin = ({navigation}) => {
             return;
         }
         saveData();
-        
         console.log(userName);
     }
-
     return (
         <View style={styles.container}>
-            
-    <ImageBackground source={image1} style={styles.image}>
+        <ImageBackground source={image1} style={styles.image}>
     
             <Text style={styles.loginTextStyle}>
                Login To Notes App
@@ -50,7 +44,6 @@ const userLogin = ({navigation}) => {
             </ImageBackground>
         </View>  
     )
-
 }
 
 const styles = StyleSheet.create({
